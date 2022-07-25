1e970a57aed7f1620707a2e37ca04402c6ecf9fe
koushil-mankali <koushilmankali@gmail.com>
2022-07-25 12:54:06 +0530

    intial commit v2

---
+++ components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import css from './Footer.module.css'
+
+const Footer = () => {
+    return <div className={css.footer}>
+        <div><span className={css.txt1}>Made in ❤️ with India by</span> <span className={css.txt2}>Koushil Mankali</span></div>
+    </div>
+}
+
+export default Footer
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import Footer from '../components/Footer/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
